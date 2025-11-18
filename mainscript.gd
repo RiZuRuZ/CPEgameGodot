e3b2e89b5ea2fae20118f9beb7192ba592ae8f88
ptka <phuree.teekapak@gmail.com>
2025-11-18 21:25:44 +0700

    wave text

--- mainscript.gd
+++ mainscript.gd
@@ -4,7 +4,7 @@ extends Node2D
 const PlayerScene = preload("res://Animation5+3/Soldier.tscn")
 const Slimescene = preload("res://Animation5+3/Slime.tscn")
 const Skeletonscene = preload("res://Animation5+3/Skeleton.tscn")
-var wave = 1
+var wave = 0
 var rng = RandomNumberGenerator.new()
 
 @export var Wave_time:int
@@ -12,15 +12,21 @@ var rng = RandomNumberGenerator.new()
 @export var SafeZone:int
 @export var SpawnZone:int
 
-func EnemieSpawn (wave) -> void:
+func EnemieSpawn () -> void:
+	wave += 1
+	print(wave)
+	$"/root/Wave".wave = wave
+	$wavetime.start()
 	var total = 5*wave
 	var Spawn_gap = Wave_time/total #second
 	if Spawn_gap > 3:
 		Spawn_gap = 3
-	var Num_slime = floor(total*(0.7-0.2*wave))
+	var Num_slime = floor(total*(0.7-0.2*(wave-1)))
 	print(Num_slime)
 	var Num_skeleton = total-Num_slime
 	print(Num_skeleton)
+	
+	
 	for i in Num_slime: #slime
 		var Slime = Slimescene.instantiate()
 		var prefix = rng.randf()
@@ -71,11 +77,14 @@ func EnemieSpawn (wave) -> void:
 			print(Skeleton.position)
 			print("spawn skeleton")
 		await get_tree().create_timer(Spawn_gap).timeout
-	wave += 1
-	$wavetime.start()
+	
+	
 
 
 func _ready() -> void:
+	var label = $"/root/Wave"
+	label.visible = true
+	label.wave = wave
 	var player = PlayerScene.instantiate()
 	add_child(player)
 	player.add_to_group("player")
@@ -84,13 +93,14 @@ func _ready() -> void:
 	$wavetime.wait_time = Wave_time
 	$first.start()
 	
+	
 
 	# optional if your scene structure matches
 	#player.gfx_path = "CharacterBody2D/Soldier"
 	#player.anim_path = "CharacterBody2D/AnimationPlayer"
 	
 func _on_first_timeout() -> void: #1st wave
-	EnemieSpawn(wave)
+	EnemieSpawn()
 	
 
 
@@ -153,4 +163,10 @@ func _on_wavetime_timeout() -> void:
 			#await get_tree().create_timer(1).timeout
 		#$wavetime.start()
 	#wave+=1
-	EnemieSpawn(wave)
+	#$"/root/Wave".wave = wave
+	EnemieSpawn()
+	
+	
+func _physics_process(delta: float) -> void:
+	$"/root/Wave".nextwave = $wavetime.time_left
+	
