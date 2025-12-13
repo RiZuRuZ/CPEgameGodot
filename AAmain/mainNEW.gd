5f7b5a5693ac447538c8f9cdd9d1296423971dc8
PonaPann <jj0959596763@gmail.com>
2025-12-13 15:16:54 +0700

    Fix regen

--- AAmain/mainNEW.gd
+++ AAmain/mainNEW.gd
@@ -160,11 +160,12 @@ func _physics_process(delta: float) -> void:
 		$"/root/Wave/CanvasLayer/Label".visible = false
 		$"/root/Wave/CanvasLayer/time".visible = false
 	if monster == 0 and current_stage == 1 and current_wave > 3:
-		print("Stage", current_stage, "Complete!")
+		print("Stage ", current_stage, " Complete!")
 		current_stage +=1
 		$"/root/LevelSave".SaveMutihealth = $"/root/LevelSave".Mutihealth
 		$"/root/LevelSave".SaveMutidam = $"/root/LevelSave".Mutidam
 		$"/root/LevelSave".SaveMutispeed = $"/root/LevelSave".Mutispeed
+		$"/root/LevelSave".SaveMutiregen = $"/root/LevelSave".Mutiregen
 		$"/root/LevelSave".SaveLevel = $"/root/LevelSave".level
 		$"/root/Wave".state = current_stage
 		$"/root/Wave/CanvasLayer/Label".visible = false
